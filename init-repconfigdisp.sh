6f8cc566f08f5e9c7ef814d79f6afc9c5285248c
MarioBarbozaA <marionetabar1@gmail.com>
2023-04-20 19:34:34 -0600

    limpieza de compose

--- init-repconfigdisp.sh
+++ init-repconfigdisp.sh
@@ -9,8 +9,7 @@ rs.initiate(
     configsvr: true,
     members: [
       { _id : 0, host : "10.0.0.3:27017" },
-      { _id : 1, host : "10.0.0.4:27017" },
-      { _id : 2, host : "10.0.0.5:27017" }
+      { _id : 1, host : "10.0.0.4:27017" }
     ]
   }
 );
